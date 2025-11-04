d2dad6ae0eb735cc8d999d193404f54793db8088
nharsch <nigelharsch@gmail.com>
2025-11-03 21:51:00 -0800

    demo with staff and departmnts

--- src/main/datascript_app/core.cljs
+++ src/main/datascript_app/core.cljs
@@ -3,79 +3,185 @@
             [uix.core :as uix :refer [defui $]]
             [uix.dom]))
 
-(def schema {:person/name {:db/unique :db.unique/identity}
-             :person/age {}
-             :person/friend {:db/valueType :db.type/ref
-                            :db/cardinality :db.cardinality/many}})
 
-(defonce db (d/create-conn schema))
 
-(defn add-person! [name age]
-  (println "Added person:" name "age:" age)
-  (d/transact! db [{:person/id (random-uuid)
-                    :person/name name
-                    :person/age (js/parseInt age)}]))
+;; TODO:
+;;
+;; define reusable queries
+;; - staff/managed-staff
+;; - department/staff
+;;
+;; staff selection should be multi select
+;; keep track of selected state on the department
+;; - department/selected true
 
-(defn get-all-people [db]
-  (d/q '[:find ?name ?age
+(def app-schema
+  ;; todo: how to define staff as a set of people
+  {
+   :db/ident {:db/unique :db.unique/identity}
+   ;; :staff/id {:db/unique :db.unique/identity}
+   :staff/name {:db/unique :db.unique/identity}
+   ;; TODO: if possible, I'd like to use the department name as the ref
+   :staff/department {:db/valueType :db.type/ref}
+
+   :department/name {:db/unique :db.unique/identity}
+   :department/manager {:db/valueType :db.type/ref}
+
+   :app/staff {:db/valueType :db.type/ref :db/cardinality :db.cardinality/many}
+   :app/departments {:db/valueType :db.type/ref :db/cardinality :db.cardinality/many}
+   :app/selected-department {:db/valueType :db.type/ref
+                             :db/cardinality :db.cardinality/one}
+   })
+
+(defonce db (d/create-conn app-schema))
+
+(defn add-staff! [name dep-name]
+  (println "Added staff" name)
+  (d/transact! db [{:staff/id (random-uuid)
+                    :staff/name name
+                    :staff/department [:department/name  dep-name]}]
+               ))
+
+(defn add-department! [name]
+  (println "Added department" name)
+  (d/transact! db [{:department/name name}]))
+
+(defn get-all-staff [db]
+  (d/q '[:find ?id ?name ?depname
          :where
-         [?e :person/name ?name]
-         [?e :person/age ?age]]
+         [?e :staff/name ?name]
+         [?e :staff/id ?id]
+         [?e :staff/department ?depid]
+         [?depid :department/name ?depname]
+         ]
        db))
 
+(defn get-selected-staff [db]
+  (d/q '[:find ?id ?name
+         :where
+         [?e :staff/name ?name]
+         [?e :staff/id ?id]
+         [?e :staff/department ?depid]
+         [?app :db/ident :app/singleton]
+         [?app :app/selected-department ?depid]]
+       db))
 
-(defui add-person-form []
+(get-selected-staff @db)
+
+(defn get-all-departments [db]
+  (d/q '[:find ?name
+         :where
+         [?e :department/name ?name]
+         ]
+       db))
+
+(defn set-selected-department! [dep]
+  (d/transact! db [{:db/ident :app/singleton
+                    :app/selected-department [:department/name dep]}])
+  )
+
+(defn clear-selected-department! []
+  (d/transact! db [[:db/retract [:db/ident :app/singleton] :app/selected-department]]))
+
+(defui add-staff-form []
   (let [[name set-name!] (uix/use-state "")
-        [age set-age!] (uix/use-state "")
+        [department set-department!] (uix/use-state "")
         handle-submit (fn [e]
                         (.preventDefault e)
-                        (when (and (seq name) (seq age))
-                          (add-person! name age)
+                        (when (seq name)
+                          (add-staff! name department)
+                          ;; TODO: gotta be a better way to reset form
                           (set-name! "")
-                          (set-age! "")))]
+                          (set-department! "")))]
     ($ :div
-       ($ :h2 "Add Person")
+       ($ :h2 "Add Staff")
        ($ :form {:on-submit handle-submit}
           ($ :div
              ($ :label "Name: ")
              ($ :input {:type "text"
-                       :value name
-                       :on-change #(set-name! (.. % -target -value))}))
-          ($ :div
-             ($ :label "Age: ")
-             ($ :input {:type "number"
-                       :value age
-                       :on-change #(set-age! (.. % -target -value))}))
-          ($ :button {:type "submit"} "Add Person")))))
+                        :value name
+                        :on-change #(set-name! (.. % -target -value))}))
+          ($ :button {:type "submit"} "Add Staff")))))
 
-(defui all-people []
-  (let [[people set-people!] (uix/use-state (get-all-people @db))]
+(defui staff-list []
+  (let [[staff set-staff!] (uix/use-state (get-selected-staff @db))]
     (uix/use-effect
       (fn []
         ;; TODO: should only recalculate this when the query changes
         (let [listener-key (d/listen! db (fn [tx-report]
-                                          (set-people! (get-all-people @db))))]
+                                          (set-staff! (get-selected-staff @db))))]
           #(d/unlisten! db listener-key)))
       [])
     ($ :div
-       ($ :h2 "All People")
+       ($ :h2 "Staff")
        ($ :ul
-          (for [[name age] people]
-            ($ :li {:key name} name " (age: " age ")"))))))
+          (for [[id name] staff]
+            ($ :li {:key id} name))))))
+
+(defui all-departments []
+  (let [[departments set-departments!] (uix/use-state (get-all-departments @db))]
+    (uix/use-effect
+      (fn []
+        ;; TODO: should only recalculate this when the query changes
+        (let [listener-key (d/listen! db (fn [tx-report]
+                                          (set-departments! (get-all-departments @db))))]
+          #(d/unlisten! db listener-key)))
+      [])
+    ($ :div
+       ($ :h2 "Departments")
+       ($ :ul
+          (for [[name] departments]
+            ($ :li {:key name} name))))))
 
 (defui app []
   ($ :div
      ($ :h1 "DataScript App with UIX2")
-     ($ add-person-form)
-     ($ all-people)))
+     ($ all-departments)
+     ($ staff-list)
+     ($ add-staff-form)
+     ))
 
 (defonce root (uix.dom/create-root (js/document.getElementById "app")))
 
 
+(comment
+  (run! add-department! ["Facilities"
+                         "Sales"
+                         "Operations"
+                         "IT"
+                         ])
+  (get-all-departments @db)
+  (run! add-staff! ["Alice" "Facilities"
+                    "Bob" "IT"
+                    "Mary" "Sales"
+                    ])
+  ;; TODO: but this does not
+  (add-staff! "Alice" "Facilities")
+  (get-all-staff @db)
+  (set-selected-department! "IT")
+  (set-selected-department! nil)
+  (d/q '[:find ?name
+         :where
+         [_ :app/selected-department ?dep]
+         [?dep :department/name ?name]
+         ]
+       @db)
+  )
+
+
 (defn init []
-  (js/console.log "DataScript app with UIX2 starting...")
-  (add-person! "Alice" 30)
-  (add-person! "Bob" 25)
+  (js/console.log "starting...")
+  (run! add-department! ["Facilities"
+                         "Sales"
+                         "Operations"
+                         "IT"
+                         ])
+  (run! #(apply add-staff! %) [["Alice" "Facilities"]
+                               ["Benedict" "Operations"]
+                               ["Sonya" "Sales"]
+                               ["Bob" "IT"]
+                               ["Mary" "Sales"]])
+
   (uix.dom/render-root ($ app) root))
 
 (init)
