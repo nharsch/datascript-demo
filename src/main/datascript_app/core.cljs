9d44e838e21b98dd9d000d7c322a40db308e6446
nharsch <nigelharsch@gmail.com>
2025-11-04 18:30:13 -0800

    selectable departments

--- src/main/datascript_app/core.cljs
+++ src/main/datascript_app/core.cljs
@@ -33,6 +33,16 @@
                              :db/cardinality :db.cardinality/one}
    })
 
+;; RULES: rules allow for query composition and reuse
+(def rules '[
+             [(manages-department ?manager ?dep)
+              [?dep :department/manager ?manager]]
+             [(manages-staff ?manager ?staff)
+              (manages-department ?manager ?dep)
+              [?staff :staff/department ?dep]]
+             ])
+
+
 (defonce db (d/create-conn app-schema))
 
 (defn add-staff! [name dep-name]
@@ -56,17 +66,28 @@
          ]
        db))
 
+(defn get-selected-departments [db]
+  (d/q '[:find ?name
+         :where
+         [_ :app/selected-department ?dep]
+         [?dep :department/name ?name]
+         ]
+       db))
+
 (defn get-selected-staff [db]
   (d/q '[:find ?id ?name
          :where
          [?e :staff/name ?name]
          [?e :staff/id ?id]
          [?e :staff/department ?depid]
-         [?app :db/ident :app/singleton]
-         [?app :app/selected-department ?depid]]
+         [?depid :department/enabled true]
+         ]
        db))
 
-(get-selected-staff @db)
+(comment
+  (get-selected-departments @db)
+  (get-selected-staff @db)
+  )
 
 (defn get-all-departments [db]
   (d/q '[:find ?name
@@ -100,38 +121,96 @@
              ($ :label "Name: ")
              ($ :input {:type "text"
                         :value name
-                        :on-change #(set-name! (.. % -target -value))}))
+                        :on-change #(set-name! (.. % -target -value))})
+             ($ :label " Department: ")
+             ($ :select {:value department
+                         :on-change #(set-department! (.. % -target -value))}
+                ($ :option {:value ""} "-- Select Department --")
+                (for [[dep-name] (get-all-departments @db)]
+                  ($ :option {:key dep-name :value dep-name} dep-name)))
+             )
           ($ :button {:type "submit"} "Add Staff")))))
+
+
+(defn department-enabled? [dep-name]
+  (boolean
+   (ffirst
+    (d/q '[:find ?enabled
+           :in $ ?dep-name
+           :where
+           [?e :department/name ?dep-name]
+           [?e :department/enabled ?enabled]]
+         @db dep-name))))
+
+(comment
+  (department-enabled? "IT")
+  (department-enabled? "Operations")
+  (enabled-deparments)
+  )
+
+(defn toggle-department-enabled! [dep-name]
+  (if (department-enabled? dep-name)
+    (d/transact! db [[:db/retract [:department/name dep-name] :department/enabled true]])
+    (d/transact! db [{:department/name dep-name
+                      :department/enabled true}])))
+
+(defn enabled-deparments []
+  (d/q '[:find ?name
+         :where
+         [?e :department/name ?name]
+         [?e :department/enabled true]]
+       @db))
+
+
+
+(defui department-pill [{:keys [dep-name]}]
+  (let [[enabled set-enabled!] (uix/use-state (department-enabled? dep-name))
+        handle-click (fn [e]
+                       (.preventDefault e)
+                       (js/console.log "Toggling department" dep-name)
+                       (toggle-department-enabled! dep-name))]
+    (uix/use-effect
+     (fn []
+       (let [listener-key (d/listen! db (fn [tx-report]
+                                          (set-enabled! (department-enabled? dep-name))))]
+         #(d/unlisten! db listener-key)))
+     [dep-name])
+    ($ :button {:on-click handle-click
+                :style {:background-color (if enabled "lightgreen" "lightgray")}}
+       dep-name)))
+
+
 
 (defui staff-list []
   (let [[staff set-staff!] (uix/use-state (get-selected-staff @db))]
     (uix/use-effect
-      (fn []
-        ;; TODO: should only recalculate this when the query changes
-        (let [listener-key (d/listen! db (fn [tx-report]
+     (fn []
+       ;; TODO: should only recalculate this when the query changes
+       (let [listener-key (d/listen! db (fn [tx-report]
                                           (set-staff! (get-selected-staff @db))))]
-          #(d/unlisten! db listener-key)))
-      [])
+         #(d/unlisten! db listener-key)))
+     [])
     ($ :div
        ($ :h2 "Staff")
        ($ :ul
           (for [[id name] staff]
             ($ :li {:key id} name))))))
 
+
 (defui all-departments []
   (let [[departments set-departments!] (uix/use-state (get-all-departments @db))]
     (uix/use-effect
-      (fn []
-        ;; TODO: should only recalculate this when the query changes
-        (let [listener-key (d/listen! db (fn [tx-report]
+     (fn []
+       ;; TODO: should only recalculate this when the query changes
+       (let [listener-key (d/listen! db (fn [tx-report]
                                           (set-departments! (get-all-departments @db))))]
-          #(d/unlisten! db listener-key)))
-      [])
+         #(d/unlisten! db listener-key)))
+     [])
     ($ :div
        ($ :h2 "Departments")
        ($ :ul
           (for [[name] departments]
-            ($ :li {:key name} name))))))
+            ($ :li {:key name} ($ department-pill {:dep-name name})))))))
 
 (defui app []
   ($ :div
@@ -166,6 +245,22 @@
          [?dep :department/name ?name]
          ]
        @db)
+  ;; TODO: Claude, is it possible to make these composable?
+  (d/q '[:find ?department-name
+         :where
+         [?manager-id :staff/name "Benedict"]
+         [?dep :department/manager ?manager-id]
+         [?dep :department/name ?department-name]
+         ]
+       @db)
+  ;; TODO: Claude, I'm doing the above query again here, I'd like to do something like :staff/managed-deps
+  (d/q '[:find ?staff-name
+         :where
+         [?manager-id :staff/name "Benedict"]
+         [?dep :department/manager ?manager-id]
+         [?staff-id :staff/department ?dep]
+         [?staff-id :staff/name ?staff-name]]
+       @db)
   )
 
 
@@ -174,14 +269,14 @@
   (run! add-department! ["Facilities"
                          "Sales"
                          "Operations"
-                         "IT"
-                         ])
+                         "IT"])
   (run! #(apply add-staff! %) [["Alice" "Facilities"]
                                ["Benedict" "Operations"]
                                ["Sonya" "Sales"]
                                ["Bob" "IT"]
                                ["Mary" "Sales"]])
-
+  (d/transact! db [{:department/name "IT"
+                    :department/manager [:staff/name "Benedict"]}])
   (uix.dom/render-root ($ app) root))
 
 (init)
